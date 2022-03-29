2aa8bb8a1dbe4f0ea710cefcfe189576660d044e
100katty <ana.hernandez@hackademy.mx>
2022-03-28 19:36:31 -0600

    icon

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Header
-         title="Pick decoraciones"
+         title="Catherine's Decoration"
          description= "Ecommerce de venta de muebles y decoraciones"
       />
       <HomePage/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import logo from '../assets/logo1.jpg'
+import { IoIosSearch, IoMdCart} from 'react-icons/io';
 
 export default function Header({title,description}) {
   return (
     <div className='header'>
-        <img src={logo} className="app-logo" alt="logo" />
-          <div className='content'>
-              <h1 className='title'>{title}</h1>
-              <p className='text'>{description}</p>
+         <div className='content-header'>
+           <img src={logo} className="app-logo" alt="logo" />
+              <div className='title-header'>
+                <h1 className='title'>{title}</h1>
+                <p className='text'>{description}</p>
+              </div>
+              <div>
+                <IoIosSearch className="icon-header"/>
+                <input  className='input-header' type="text" placeholder="SEARCH" />
+                <IoMdCart className="icon-header"/>
+              </div>
+              
           </div>
     </div>
   )
